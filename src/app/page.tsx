181cab8c0f66657bd4c78c664b5ffd49d6b077fe
MSRayed <msrayed2006@gmail.com>
2025-07-02 21:40:37 +0600

    create landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-export default function Home() {
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function LandingPage() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex flex-col gap-5 justify-center items-center h-screen">
+      <h1 className="text-6xl font-extrabold text-primary">Bio Cards</h1>
+      <p className="text-xl">Generate cards from biology terms</p>
+
+      <Link href="/home">
+        <Button className="text-lg mt-8">Get started</Button>
+      </Link>
     </div>
   );
 }
